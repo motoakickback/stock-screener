a4620b9e8510be125b3c8d05256783e645400209
motoakickback <motoakickback@gmail.com>
2026-03-22 09:28:19 +0900

    Update app.py

--- app.py
+++ app.py
@@ -1173,11 +1173,13 @@ with tab2:
                             """, unsafe_allow_html=True)
                         # -------------------------------------------------------------
                         
+                        # -------------------------------------------------------------
                         # --- テクニカルレーダーとチャート描画 ---
                         df_chart, bt_chart, tp5_c, tp10_c, tp15_c, tp20_c = charts_data[r['Code']]
                         df_chart = calc_technicals(df_chart) # 計器計算
                         st.markdown(render_technical_radar(df_chart, bt_chart, st.session_state.bt_tp), unsafe_allow_html=True)
                         draw_chart(df_chart, bt_chart, tp5_c, tp10_c, tp15_c, tp20_c)
+                        # -------------------------------------------------------------
 
 with tab3:
     st.markdown('<h3 style="font-size: clamp(14px, 4.5vw, 24px); margin-bottom: 1rem;">📉 鉄の掟：一括バックテスト</h3>', unsafe_allow_html=True)
